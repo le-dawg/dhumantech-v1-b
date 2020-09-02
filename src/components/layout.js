6990e8bb00e9334d141f5094fff8dffbace0e686
le-dawg <dawid.golebiewski@tuhh.de>
2020-09-02 08:41:36 -0700

    1st deploy on brainnet brnach. new logo test and placement

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "gatsby"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
-import LogoImg from '../../static/logo-brainnet.svg';
+import LogoImg from '../../static/logo-biturnal.svg';
 
 class Layout extends Component {
   render() {
